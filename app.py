17b087f08c03c8d183876d81a1461826e3aa28b8
nanatny <nathany.snogueira@gmail.com>
2024-01-03 21:59:37 -0300

    Ajustando layouts e páginas de convênios

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__, static_folder= "static", template_folder= "templates")
 
@@ -9,3 +9,30 @@ def index():
 @app.route("/sindicato")
 def sindicato():
     return render_template("sindicato.html", context={})
+
+@app.route("/convenios")
+def convenios():
+    return render_template("convenios.html", context={})
+
+@app.route("/convenios/<pagina>")
+def convenios_detalhes(pagina):
+    paginas = ["beleza", "comercio", "educacao", "lazer", "saude", "viagem"]
+    if pagina in paginas:
+        return render_template(f"convenios/{pagina}.html", context={})
+    return redirect(url_for("index"))
+
+@app.route("/convencoes")
+def convencoes():
+    return render_template("convencoes.html", context={})
+
+@app.route("/noticias")
+def noticias():
+    return render_template("noticias.html", context={})
+
+@app.route("/carteirinha")
+def carteirinha():
+    return render_template("carteirinha_virtual.html", context={})
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html", context={})
